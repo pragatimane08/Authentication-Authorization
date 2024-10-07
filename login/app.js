1ba4d2bb4029dcc11d0b2b45c16552027b67fc41
Pragati Narayan Mane <118499416+pragatimane08@users.noreply.github.com>
2024-10-07 13:32:14 +0530

    Delete login directory

--- login/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authRoutes = require('./routes/authrought');
-const app = express();
-
-app.use(express.json()); // Ensure this line is present
-
-app.use('/auth', authRoutes);
-
-module.exports = app;
